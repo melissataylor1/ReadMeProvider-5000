d092542039a247c9e18719eb851d8316481a51d0
m1taylor <m1taylor@ryerson.ca>
2023-02-08 18:20:17 -0500

    added to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
+    
     {
         type: 'input',
         name: 'projecttitle',
