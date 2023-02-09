24f449bbd15866fabbd7ff0eac44c83e77b427aa
m1taylor <m1taylor@ryerson.ca>
2023-02-08 19:23:18 -0500

    made render liscense section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -35,10 +35,40 @@ function renderLicenseLink(license) {
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+  if (license === 'MIT') {
+    return license = 'https://img.shields.io/badge/License-MIT-yellow.svg'
+  }
+  if (license === 'Apache 2.0') {
+    return license = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg'
+  }
+  if (license === 'GNU GPL v3') {
+    return license = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg' 
+  }
+  if( license === 'MPL 2.0'){
+  return 'https://opensource.org/licenses/MPL-2.0'
+  }
+  if (license === "None") {
+    section = "";
+  } 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
 
+  [${data.license}]${renderLicenseLink(data.license)}
+
+  ##  Description
+  ${data.description}
+  ${data.description}
+  ${data.description}
+  ${data.description}
+
+
+`;
+}
+
+module.exports = generateMarkdown;
